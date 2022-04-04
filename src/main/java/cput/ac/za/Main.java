9c9a73c425d35e4948c53a23db32b6f014732e76
juicyj201 <220102473@mycput.ac.za>
2022-04-04 12:09:16 +0200

    Initial commit

---
+++ src/main/java/cput/ac/za/Main.java
@@ -0,0 +1,13 @@
+package cput.ac.za;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cput/ac/za/MainTest.java
@@ -0,0 +1,15 @@
+package cput.ac.za;
+
+import org.junit.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple Main.
+ */
+public class MainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+}
