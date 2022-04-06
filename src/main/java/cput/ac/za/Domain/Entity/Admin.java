143dea4099349882be8ef994065cb27bbf15dfb0
juicyj201 <220102473@mycput.ac.za>
2022-04-06 13:55:30 +0200

    Removed seperate builder classes and abstract builder additionally

--- src/main/java/cput/ac/za/Domain/Entity/Admin.java
+++ src/main/java/cput/ac/za/Domain/Entity/Admin.java
@@ -1,25 +1,67 @@
 package cput.ac.za.Domain.Entity;
 
+/**
+ * Joshua Julies
+ * 220102473
+ * The admin entity
+ */
+
 public class Admin {
     public String adminID;
     public String adminType;
     public String adminFirstName;
     public String adminSurname;
 
-    public void setAdminID(String adminID){
+    public Admin(AdminBuilder adminBuilder){
         this.adminID = adminID;
-    }
-
-    public void setAdminType(String adminType){
         this.adminType = adminType;
-    }
-
-    public void setAdminFullName(String adminFirstName, String adminSurname){
         this.adminFirstName = adminFirstName;
         this.adminSurname = adminSurname;
     }
 
-    public Admin getAdmin(Admin admin){
-        return admin;
+    public String getAdminID(String adminID){
+        return adminID;
+    }
+
+    public String getAdminType(String adminType){
+        return adminType;
+    }
+
+    public String getAdminFullName(String adminFirstName, String adminSurname){
+        return adminFirstName + adminSurname;
+    }
+
+    public static class AdminBuilder{
+        public String adminID;
+        public String adminType;
+        public String adminFirstName;
+        public String adminSurname;
+
+        public AdminBuilder(String adminID, String adminFirstName, String adminSurname){
+            this.adminID = adminID;
+            this.adminFirstName = adminFirstName;
+            this.adminSurname = adminSurname;
+        }
+
+        public AdminBuilder setAdminID(String adminID){
+            this.adminID = adminID;
+            return this;
+        }
+
+        public AdminBuilder setAdminType(String adminType){
+            this.adminType = adminType;
+            return this;
+        }
+
+        public AdminBuilder setAdminFullName(String adminFirstName, String adminSurname){
+            this.adminFirstName = adminFirstName;
+            this.adminSurname = adminSurname;
+            return this;
+        }
+
+        public Admin buildAdmin(){
+            Admin admin = new Admin(this);
+            return admin;
+        }
     }
 }
--- src/main/java/cput/ac/za/Domain/Entity/Payment.java
+++ src/main/java/cput/ac/za/Domain/Entity/Payment.java
@@ -1,28 +1,74 @@
 package cput.ac.za.Domain.Entity;
 
+/**
+ * Joshua Julies
+ * 220102473
+ * The Payment entity
+ */
+
 public class Payment {
     public String paymentID;
     public String studentAccountID;
     public String paymentDate;
     public int paymentAmount;
 
-    public void setPaymentID(String paymentID){
+    public Payment(PaymentBuilder paymentBuilder){
         this.paymentID = paymentID;
-    }
-
-    public void setStudentAccountID(String studentAccountID){
         this.studentAccountID = studentAccountID;
-    }
-
-    public void setPaymentDate(String paymentDate){
         this.paymentDate = paymentDate;
-    }
-
-    public void setPaymentAmount(int paymentAmount){
         this.paymentAmount = paymentAmount;
     }
 
-    public Payment getPayment(Payment payment){
-        return payment;
+    public String getPaymentID(String paymentID){
+        return paymentID;
+    }
+
+    public String getStudentAccountID(String studentAccountID){
+        return studentAccountID;
+    }
+
+    public String setPaymentDate(String paymentDate){
+        return paymentDate;
+    }
+
+    public int getPaymentAmount(int paymentAmount){
+        return paymentAmount;
+    }
+
+    public static class PaymentBuilder{
+        public String paymentID;
+        public String studentAccountID;
+        public String paymentDate;
+        public int paymentAmount;
+
+        public PaymentBuilder(String paymentID, int paymentAmount){
+            this.paymentID = paymentID;
+            this.paymentAmount = paymentAmount;
+        }
+
+        public PaymentBuilder setPaymentID(String paymentID){
+            this.paymentID = paymentID;
+            return this;
+        }
+
+        public PaymentBuilder setStudentAccountID(String studentAccountID){
+            this.studentAccountID = studentAccountID;
+            return this;
+        }
+
+        public PaymentBuilder setPaymentDate(String paymentDate){
+            this.paymentDate = paymentDate;
+            return this;
+        }
+
+        public PaymentBuilder setPaymentAmount(int paymentAmount){
+            this.paymentAmount = paymentAmount;
+            return this;
+        }
+
+        public Payment buildPayment(){
+            Payment payment = new Payment(this);
+            return payment;
+        }
     }
 }
