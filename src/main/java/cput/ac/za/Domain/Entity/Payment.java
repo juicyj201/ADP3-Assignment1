f1d24b8248c6c7f7801c6cdfde5bad3a8f785f6c
juicyj201 <220102473@mycput.ac.za>
2022-04-07 17:49:10 +0200

    Created factory classes, added repository interface and implementations, as well as seperate builder and director classes

--- src/main/java/cput/ac/za/Domain/Entity/Payment.java
+++ src/main/java/cput/ac/za/Domain/Entity/Payment.java
@@ -12,63 +12,43 @@ public class Payment {
     public String paymentDate;
     public int paymentAmount;
 
-    public Payment(PaymentBuilder paymentBuilder){
+    public Payment(){
         this.paymentID = paymentID;
         this.studentAccountID = studentAccountID;
         this.paymentDate = paymentDate;
         this.paymentAmount = paymentAmount;
     }
 
-    public String getPaymentID(String paymentID){
+    public void setPaymentID(String paymentID){
+        this.paymentID = paymentID;
+    }
+
+    public void setStudentAccountID(String studentAccountID){
+        this.studentAccountID = studentAccountID;
+    }
+
+    public void setPaymentDate(String paymentDate){
+        this.paymentDate = paymentDate;
+    }
+
+    public void setPaymentAmount(int paymentAmount){
+        this.paymentAmount = paymentAmount;
+    }
+
+    public String getPaymentID(){
         return paymentID;
     }
 
-    public String getStudentAccountID(String studentAccountID){
+    public String getStudentAccountID(){
         return studentAccountID;
     }
 
-    public String setPaymentDate(String paymentDate){
+    public String getPaymentDate(){
         return paymentDate;
     }
 
-    public int getPaymentAmount(int paymentAmount){
+    public int getPaymentAmount(){
         return paymentAmount;
     }
 
-    public static class PaymentBuilder{
-        public String paymentID;
-        public String studentAccountID;
-        public String paymentDate;
-        public int paymentAmount;
-
-        public PaymentBuilder(String paymentID, int paymentAmount){
-            this.paymentID = paymentID;
-            this.paymentAmount = paymentAmount;
-        }
-
-        public PaymentBuilder setPaymentID(String paymentID){
-            this.paymentID = paymentID;
-            return this;
-        }
-
-        public PaymentBuilder setStudentAccountID(String studentAccountID){
-            this.studentAccountID = studentAccountID;
-            return this;
-        }
-
-        public PaymentBuilder setPaymentDate(String paymentDate){
-            this.paymentDate = paymentDate;
-            return this;
-        }
-
-        public PaymentBuilder setPaymentAmount(int paymentAmount){
-            this.paymentAmount = paymentAmount;
-            return this;
-        }
-
-        public Payment buildPayment(){
-            Payment payment = new Payment(this);
-            return payment;
-        }
-    }
 }
