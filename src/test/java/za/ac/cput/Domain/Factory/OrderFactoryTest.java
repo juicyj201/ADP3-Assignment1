b66822b3d765adc0ad3fc1550910072ba25cb176
dellie_Yah <delronclaassen@gmail.com>
2022-04-11 11:00:32 +0200

    Test classes created for the meal and order entity, meal and order factory, and for the meal and order interface implementations.

---
+++ src/test/java/za/ac/cput/Domain/Factory/OrderFactoryTest.java
@@ -0,0 +1,19 @@
+package za.ac.cput.Domain.Factory;
+
+/**
+ * Delron Chad Claassen
+ * 219360561
+ * The order factory test */
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.Domain.Entity.Order;
+
+public class OrderFactoryTest {
+    @Test
+    public void test(){
+        Order order = OrderFactory.createOrder("ORD001", "STAC001", "Hamburger", "ORNUM001", "Hamburger with large chips.", "####RECEIPT#######");
+        System.out.println(order.toString());
+        Assertions.assertNotNull(order);
+    }
+}
