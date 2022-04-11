b62cd426eb208c1042877ab6b6b3f50c90464a01
dellie_Yah <delronclaassen@gmail.com>
2022-04-11 10:20:29 +0200

    Order and Meal factory classes created.

---
+++ src/main/java/za/ac/cput/Domain/Factory/MealFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.Domain.Factory;
+
+/**
+ * Delron Chad Claassen
+ * 219360561
+ * The Order Factory
+ */
+
+import za.ac.cput.Domain.Entity.Meal;
+
+public class MealFactory
+{
+    public static Meal createMeal(String orderId, String studentMealChoice, String drink, String allergens)
+    {
+        if(orderId == null)
+            return null;
+
+        return new Meal.MealBuilder().setOrderId(orderId)
+                .setStudentMealChoice(studentMealChoice)
+                .setDrink(drink)
+                .setAllergens(allergens)
+                .build();
+    }
+}
---
+++ src/main/java/za/ac/cput/Domain/Factory/OrderFactory.java
@@ -0,0 +1,25 @@
+package za.ac.cput.Domain.Factory;
+
+/**
+ * Delron Chad Claassen
+ * 219360561
+ * The Order Factory
+ */
+
+import za.ac.cput.Domain.Entity.Order;
+
+public class OrderFactory {
+    public static Order createOrder(String orderId, String studentAccountId, String studentMealChoice, String orderNum, String orderDesc, String orderReceipt)
+    {
+        if(orderId == null)
+            return null;
+
+        return new Order.OrderBuilder().setOrderId(orderId)
+                .setStudentAccountId(studentAccountId)
+                .setStudentMealChoice(studentMealChoice)
+                .setOrderNum(orderNum)
+                .setOrderDesc(orderDesc)
+                .setOrderReceipt(orderReceipt)
+                .build();
+    }
+}
