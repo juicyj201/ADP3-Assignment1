8f45e84310cfc092439b5c1e7ee88e09545987b6
Delron Claassen <73716825+Dellie-Yah@users.noreply.github.com>
2022-10-03 11:50:37 +0200

    Overwrote the securityFilterChain property.

--- src/main/java/za/ac/cput/Security/WebSecurityConfig.java
+++ src/main/java/za/ac/cput/Security/WebSecurityConfig.java
@@ -1,26 +1,46 @@
 package za.ac.cput.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private AuthEntryPoint authEntryPoint;
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
+//    @Autowired
+//    private AuthEntryPoint authEntryPoint;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/securityNone").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .authenticationEntryPoint(authEntryPoint);
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/securityNone").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic()
+//                .authenticationEntryPoint(authEntryPoint);
+//
+//        http.addFilterAfter(new ResponseFilter(), ResponseFilter.class);
+//    }
 
-        http.addFilterAfter(new ResponseFilter(), ResponseFilter.class);
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers("/").permitAll()
+//                        .antMatchers("/", "/index").permitAll()
+                        .anyRequest().authenticated()
+                );
+//                .formLogin((form) -> form
+//                        .loginPage("/login")
+//                        .permitAll()
+//                )
+//                .logout((logout) -> logout.permitAll());
+
+        return http.build();
     }
 }
