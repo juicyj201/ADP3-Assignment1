4c6c25ade6f7f6821b03e5a05e8885534052e962
Keziah Robinson <keziah@kineticskunk.org>
2022-10-26 15:12:59 +0200

    Updating meal controller tests

--- src/test/java/za/ac/cput/Controller/MealControllerTest.java
+++ src/test/java/za/ac/cput/Controller/MealControllerTest.java
@@ -1,28 +1,83 @@
 package za.ac.cput.Controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import za.ac.cput.Domain.Entity.Meal;
 import za.ac.cput.Factory.MealFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MealControllerTest {
 
     private MealController mealController;
-//    private Meal newMeal = MealFactory.createMeal()
+    private Meal newMeal = MealFactory.createMeal(1000L, "Cheeseburger", "Coke", "Peanuts");
+
+    @LocalServerPort
+    private int baseport;
+    private String baseURL;
+    @Autowired
+    private TestRestTemplate restTemplate;
+
 
     @BeforeEach
     void setUp() {
         assertNotNull(mealController);
+        this.newMeal = MealFactory.createMeal(1000L, "Cheeseburger", "Coke", "Peanuts");
+        this.baseURL = "http://localhost:"+this.baseport+"/meal/";
+        System.out.println(newMeal);
     }
 
 
+    @Test
+    public void addMeal(){
+        String url = baseURL + "save/";
+        System.out.println(url);
+        try {
+            Assertions.assertNotNull(mealController.addMeal(newMeal));
+        }catch(NullPointerException npe) {
+            npe.getMessage();
+            npe.getStackTrace();
+        }
+        System.out.println(this.newMeal.getOrderId()+ ": Has been saved");}
+
+    @Test
+    public void testRead(){
+
+        try {
+            Assertions.assertNotNull(mealController.getMeal(newMeal));
+        }catch(NullPointerException nullPointerException){
+            nullPointerException.getMessage();
+            nullPointerException.getStackTrace();
+        }
+        System.out.println("Student Account Found: " + newMeal);
+    }
+
+
+    @Test
+    public void testDelete(){
+        String url = baseURL + "delete/" + this.newMeal.getOrderId();
+        this.restTemplate.delete(url);
+        System.out.println("Student Account deleted: " + this.newMeal.getOrderId()+"\n"+url);
+    }
+
+    @Test
+    public void testUpdate(){
+        String url = baseURL + "update/" + this.newMeal.getOrderId();
+        System.out.println(url);
+        this.restTemplate.put(url, newMeal);
+        System.out.println("Student Account updated: " + this.newMeal.getOrderId()+"\n"+url);
+
+
+    }
 
 
 
