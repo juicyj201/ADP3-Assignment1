2dfac1b2480f57cd6267eab2a11e721472aa6eaa
Keziah Robinson <keziah@kineticskunk.org>
2022-10-26 15:29:51 +0200

    Updating all controller tests

--- src/test/java/za/ac/cput/Controller/OrderControllerTest.java
+++ src/test/java/za/ac/cput/Controller/OrderControllerTest.java
@@ -1,26 +1,14 @@
 package za.ac.cput.Controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import za.ac.cput.Domain.Entity.Meal;
 import za.ac.cput.Domain.Entity.Order;
-import za.ac.cput.Domain.Entity.StudentAccount;
 import za.ac.cput.Factory.OrderFactory;
-import za.ac.cput.Factory.StudentAccountFactory;
-import za.ac.cput.Service.Impl.MealService;
-import za.ac.cput.Service.Impl.MealServiceImpl;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Delron Chad Claassen
@@ -49,10 +37,10 @@ public class OrderControllerTest {
 
     @Test
     public void testSave(){
-        String url = baseURL + "save/";
+        String url = baseURL + "save";
         System.out.println(url);
         try {
-            assertNotNull(orderController.addOrder(order));
+            assertNotNull(orderController.getOrder(order));
         }catch(NullPointerException npe) {
             npe.getMessage();
             npe.getStackTrace();
